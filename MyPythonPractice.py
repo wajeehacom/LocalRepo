740b83739b96f96af834ac5a6a07ebc729d38768
wajeehacom <wajeehayaseen488@gmail.com>
2026-01-26 13:19:53 +0500

    add MyPyhtonPractice in feature1

---
+++ MyPythonPractice.py
@@ -0,0 +1,9 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        if target - num in seen:
+            return [seen[target - num], i]
+        seen[num] = i
+numbers_list = [2, 7, 11, 15]
+target_value = 9
+print("sum of two target Element:",two_sum(numbers_list,target_value))
